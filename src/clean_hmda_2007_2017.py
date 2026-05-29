adf3beb0130b668fd97919f9a3489393f303db8d
Janani Dhileepan <jd2524@ic.ac.uk>
2026-05-29 15:42:03 +0100

    clean prelim dataset

--- src/clean_hmda_2007_2017.py
+++ src/clean_hmda_2007_2017.py
@@ -37,3 +37,25 @@ for filename in os.listdir(path):
 df_merge = pd.concat(flist, axis=0, ignore_index=True)
 df_merge.drop(df_merge.columns[0], axis=1) # Drop index column
 df_merge.to_csv("concat.csv")
+
+# Clean data
+# df_merge = pd.read_csv("concat.csv")
+df = df_merge.sort_values('as_of_year', kind='stable') # Reorganise year
+df = df.iloc[:, 2:].reset_index(drop=True) # Delete unnamed columns
+
+# Drop instances where action_taken == 6
+# This is when the loan is purchased by an institution
+# This has no information on individuals and is therefore not useful for us
+
+print("The percentage of entries where the loan was purchased by an institution is", round(len(df[df['action_taken'] == 6])/len(df),2))
+df = df[df.action_taken != 6] # Remove 18% of entries
+
+# ------------------------------------------------------------------------------
+# 01. Missing values
+# ------------------------------------------------------------------------------
+
+# Check missingness by column
+missing_df = df.isnull().sum(axis=0)
+print(missing_df)
+
+df.groupby("action_taken").size()
